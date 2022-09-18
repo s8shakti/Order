cb609e194ef36417dda8404e487e47959897b2f2
s8shakti <73886667+s8shakti@users.noreply.github.com>
2022-09-18 22:31:44 +0530

    initial commits

---
+++ src/main/java/com/javatechie/os/api/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.javatechie.os.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatechie.os.api.entity.Order;
+import com.javatechie.os.api.service.OrderService;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+	@Autowired
+	private OrderService service;
+	
+	@PostMapping("/bookOrder")
+	public Order bookOrder(@RequestBody Order order) {
+		
+		return service.saveOrder(order);
+	}
+}
